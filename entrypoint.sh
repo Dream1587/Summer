297c3822d7386d58d69774002f579536222fb2c1
Danny <139473855+Dream1587@users.noreply.github.com>
2023-07-13 14:06:33 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 设置各变量
-WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+WSPATH=${WSPATH:-'0d96853b-6307-44dc-bb0b-fad73d23730a'}
+UUID=${UUID:-'0d96853b-6307-44dc-bb0b-fad73d23730a'}
 
 # 其他Paas保活
 PAAS1_URL=
